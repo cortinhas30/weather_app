5e041a2087683f60d975ed76fc37ff806102ce21
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-01-04 18:27:09 -0300

    top buttons estilizacao

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import TopButtons from "./components/TopButtons";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
     </div>
   );
 }
