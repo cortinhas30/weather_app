be9abba95b8da826848c9e6117a795be4c99efcd
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-01-05 17:39:11 -0300

    Time and locations estilizacao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Inputs from "./components/Inputs";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import TimeAndLocation from "./components/TimeAndLocation";
 import TopButtons from "./components/TopButtons";
 
 function App() {
@@ -7,6 +9,9 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
       <TopButtons />
       <Inputs />
+
+      <TimeAndLocation />
+      <TemperatureAndDetails />
     </div>
   );
 }
---
+++ src/components/TemperatureAndDetails.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TemperatureAndDetails() {
+  return (
+    <div>
+        <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+            <p>Com nuvens ou qualquer porra</p>
+        </div>
+    </div>
+  )
+}
+
+export default TemperatureAndDetails
