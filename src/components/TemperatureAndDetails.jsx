5d1a3f32d2e1624b49f06506625949573e087c34
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-01-05 18:17:51 -0300

    Temperature and details estilizacao

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
+import {
+  UilArrowUp,
+  UilArrowDown,
+  UilTemperature,
+  UilTear,
+  UilWind,
+  UilSun,
+  UilSunset,
+} from "@iconscout/react-unicons";
 
 function TemperatureAndDetails() {
   return (
     <div>
-        <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
-            <p>Com nuvens ou qualquer porra</p>
+      <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
+        <p>Com nuvens ou qualquer porra</p>
+      </div>
+
+      <div className="flex flex-row items-center justify-between text-white py-3">
+        <img
+          src="http://openweathermap.org/img/wn/01d@2x.png"
+          alt=""
+          className="w-20"
+        />
+        <p className="text-5xl">34°</p>
+        <div className="flex flex-col space-y-2">
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTemperature size={18} className="mr-1"/>
+            Real fell:
+            <span className="font-medium ml-1">32°</span>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default TemperatureAndDetails
+export default TemperatureAndDetails;
