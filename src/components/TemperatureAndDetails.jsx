deb2d0310f088f382113dcf5faf10c1a0f537e33
Lucas Cortinhas <lucascortinhas.lc@gmail.com>
2023-01-05 21:08:29 -0300

    Temperature and details estilizacao final

--- src/components/TemperatureAndDetails.jsx
+++ src/components/TemperatureAndDetails.jsx
@@ -25,11 +25,46 @@ function TemperatureAndDetails() {
         <p className="text-5xl">34°</p>
         <div className="flex flex-col space-y-2">
           <div className="flex font-light text-sm items-center justify-center">
-            <UilTemperature size={18} className="mr-1"/>
+            <UilTemperature size={18} className="mr-1" />
             Real fell:
             <span className="font-medium ml-1">32°</span>
           </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTear size={18} className="mr-1" />
+            Humidity:
+            <span className="font-medium ml-1">65%</span>
+          </div>
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilWind size={18} className="mr-1" />
+            Wind:
+            <span className="font-medium ml-1">11 km/h</span>
+          </div>
         </div>
+      </div>
+
+      <div className="flex flex-row items-center justify-center space-x-2 text-white text-sm py-3">
+        <UilSun />
+        <p className="font-light">
+          Rise: <span className="font-medium ml-1">06:45 AM</span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSunset />
+        <p className="font-light">
+          Set: <span className="font-medium ml-1">07:35 PM</span>
+        </p>
+        <p className="font-light">|</p>
+        
+        <UilSun />
+        <p className="font-light">
+          High: <span className="font-medium ml-1">45°</span>
+        </p>
+        <p className="font-light">|</p>
+
+        <UilSun />
+        <p className="font-light">
+          Low: <span className="font-medium ml-1">27°</span>
+        </p>
       </div>
     </div>
   );
